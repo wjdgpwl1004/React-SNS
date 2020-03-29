00c4c2fe94d9e25a6543eed198ea7a668a0aead2
정곰 <wjdgpwl1004@naver.com>
2020-03-30 01:11:53 +0900

    프로필화면 만들기

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,10 +1,42 @@
 import React from 'react';
+import { Form, Button, List, Input, Card, Icon } from 'antd';
 
 const Profile = () => {
     return (
         <>
             <div>
-               프로필
+               <Form style={{ marginBottom: '20px', border: '1px solid #d9d9d9', padding: '20px'}}>
+                   <Input addonBefore="닉네임" />
+                   <Button type="primary">수정</Button>
+               </Form>
+               <List
+                style={{ marginBottom:'20px'}}
+                grid={{ gutter:4, xs: 2, md: 3 }}
+                size="small"
+                header={<div>팔로잉 목록</div>}
+                loadMore={<Button style={{ width: '100%' }}>더보기</Button>}
+                bordered
+                dataSource={['정곰', '이다', '노드버드']}
+                renderItem={item => (
+                    <List.Item style={{ marginTop: '20px' }}>
+                        <Card actions={[<Icon key="stop" type="stop"/>]}><Card.Meta description={item} /></Card>
+                    </List.Item>
+                )}
+               />
+               <List
+                style={{ marginBottom:'20px'}}
+                grid={{ gutter:4, xs: 2, md: 3 }}
+                size="small"
+                header={<div>팔로워 목록</div>}
+                loadMore={<Button style={{ width: '100%' }}>더보기</Button>}
+                bordered
+                dataSource={['정곰', '이다', '노드버드']}
+                renderItem={item => (
+                    <List.Item style={{ marginTop: '20px' }}>
+                        <Card actions={[<Icon key="stop" type="stop"/>]}><Card.Meta description={item} /></Card>
+                    </List.Item>
+                )}
+               />
             </div>
         </>
     );
